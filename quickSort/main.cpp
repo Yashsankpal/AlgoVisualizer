713a345c905dec871cb61c3b5116c2fb7631be5a
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2021-09-09 12:49:57 +0530

    quickSort working

--- quickSort/main.cpp
+++ quickSort/main.cpp
@@ -19,22 +19,41 @@ class Box{
   public:
   int part=0;
   vector<Data> column;
+  queue<lst> ls;
+  lst tq;
 
   //Create
   Box(int value){
     Data data;
     for(int i=0;i<value;i++){
       data.posx = i;
-      data.height = 1 + rand()%(500);
+      data.height = 1 + rand()%(400);
       column.push_back(data);
     }
   }
 
   //Swap 
-  void swap(int left,int right){
+  void bswap(int left,int right){
     swap(column[left].height,column[right].height);
   }
 
+  void qu(int low ,int high){
+    tq.low = low;
+    tq.high = high;
+    ls.push(tq);
+  }
+
+  lst front(){
+    tq = ls.front();
+    ls.pop();
+    return tq;
+  }
+
+  void bpush(int low,int high){
+    tq.low = low;
+    tq.high = high;
+    ls.push(tq); 
+  }
 };
 
 
@@ -43,19 +62,14 @@ class Box{
 int main(){
 
   //Inputs
-  int n=400;
+  int n=500;
   Box b(n);
   // bool sort = false;
 
-  int low,high,pivot,i,j;
+  int low,high,pivot,i,j,part;
   low=high=pivot=0;
   
-  queue<lst> qu;
-  lst ls;
-
-  ls.low = 0;
-  ls.high = n-1;
-  qu.push(ls);
+  b.qu(0,n-1);
 
 
 
@@ -81,46 +95,39 @@ int main(){
       if(appEvent.type == Event::Closed) appWindow.close();
     }
 
-    if(!qu.empty()){
+    if(!b.ls.empty()){
       
-      lst tq = qu.front();
-      qu.pop();
+      lst tq = b.front();
       
-      pivot = b.column[tq.high].height; // pivot 
-      i = (tq.low - 1); // Index of smaller element and indicates the right position of pivot found so far
+      low = tq.low;
+      high = tq.high;
+      pivot = b.column[high].height; // pivot 
+      i = low; // Index of smaller element and indicates the right position of pivot found so far
     
-      for (j = tq.low; j < tq.high; j++) 
+      for (j = low; j < high; j++) 
       { 
           // If current element is smaller than the pivot 
           if (b.column[j].height < pivot) 
           { 
+              b.bswap(i,j); 
               i++; // increment index of smaller element 
-              b.swap(i,j); 
           } 
       }
 
-      b.swap(i + 1, tq.high); 
-
-      lst ts;
-      if(i != tq.low){
-        ts.low = tq.low;
-        ts.high = i;
-        qu.push(ts);
+      b.bswap(i, high); 
+      part = i;
+      if(low < high){
+        b.qu(low,part-1);
+        b.qu(part+1,high);
       }
-      if(i+1 != tq.high){
-        ts.low = i+2;
-        ts.high = tq.high;
-        qu.push(ts);
-      }
-
     }
 
 
     //PRINT
     for(auto x:b.column){
-      rectangle.setFillColor(Color(250,100,200));
+      rectangle.setFillColor(Color(250,100+x.posx,200));
       rectangle.setPosition(x.posx,600);
-      rectangle.setSize(Vector2f(1,x.height));
+      rectangle.setSize(Vector2f(2,x.height));
       rectangle.setRotation(180);
       appWindow.draw(rectangle);
     }
